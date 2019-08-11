7478a3724f3dbcfdc933d526c266a79ea7eaeafd
wilmardo <info@wilmardenouden.nl>
2019-08-11 12:51:48 +0200

    fix(entrypoint): Refactor of entrypoint
    Signed-off-by: wilmardo <info@wilmardenouden.nl>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,15 @@
 
 # Create symlinks when config mounted, else exit
 if [ -d "/config" ]; then
-    for file in /config/*; do
-        filename=$(basename $file)
-        ln -sf /config/$filename /dev/shm/$filename
-    done
+  # For each config file create a symlink
+  for file in /config/*; do
+    filename=$(basename "$file")
+    # Create symlink when it does not exist yet
+    if [ ! -L "/dev/shm/$filename" ]; then
+      echo "Creating symlink from /config/$filename to /dev/shm/$filename"
+      ln -sf "/config/$filename" "/dev/shm/$filename"
+    fi
+  done
 else
   echo "No /config found, please mount config to container"
   exit 1
@@ -15,10 +20,19 @@ fi
 # Create symlink for state.json:
 # https://github.com/Koenkk/zigbee2mqtt/commit/9396bde1f3b022e0f634487d1a37d2a5127c8cb3#diff-f68567477d803b49930337bf7fe1556bR16
 if [ -d "/data" ]; then
-    mkdir .storage
-    ln -sf /data/.storage /dev/shm/.storage
-    ln -sf /data/state.json /dev/shm/state.json
-    ln -sf /data/state.json /dev/shm/database.db
+  # Create .storage dir when not already there
+  if [ ! -d "/data/.storage" ]; then
+    echo "Creating /data/.storage directory"
+    mkdir /data/.storage
+  fi
+
+  # Create symlinks needed for persistance
+  for symlink in .storage state.json database.db; do
+    if [ ! -L "/dev/shm/$symlink" ]; then
+      echo "Creating symlink from /data/$symlink to /dev/shm/$symlink"
+      ln -sf "/data/$symlink" "/dev/shm/$symlink"
+    fi
+  done
 fi
 
 # Start zigbee2mqtt
