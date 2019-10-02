ae1d19f6c0b6a5f4274e6e4c7d44effe80787dca
wilmardo <info@wilmardenouden.nl>
2019-10-02 21:54:18 +0200

    fix: persist coordinator_backup.json to /data
    Signed-off-by: wilmardo <info@wilmardenouden.nl>

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ if [ -d "/data" ]; then
 
   # Create symlinks needed for persistance
   # devices.yml and group.yml are appended to when joining
-  for symlink in .storage state.json database.db devices.yaml groups.yaml; do
+  for symlink in .storage state.json database.db coordinator_backup.json devices.yaml groups.yaml; do
     if [ ! -L "/dev/shm/$symlink" ]; then
       echo "Creating symlink from /data/$symlink to /dev/shm/$symlink"
       ln -sf "/data/$symlink" "/dev/shm/$symlink"
